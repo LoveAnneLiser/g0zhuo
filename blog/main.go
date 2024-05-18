2d0e863c416071ef9ee7b8638d1854851686356b
LoveAnneLiser <2974728131@qq.com>
2024-05-18 19:42:02 +0800

    1.2路由实现之简单路由

---
+++ blog/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	zhuo "github.com/anneliser/g0zhuo"
+	"net/http"
+)
+
+func main() {
+
+	//http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Fprintf(writer, "%s 欢迎来到卓的Go框架", "zhuo.com")
+	//})
+	//err := http.ListenAndServe(":8111", nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	engine := zhuo.New()
+	engine.Add("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s 欢迎来到卓的Go框架", "zhuo.com")
+	})
+	engine.Run()
+}
---
+++ zhuo/go.mod
@@ -0,0 +1,3 @@
+module github.com/anneliser/g0zhuo
+
+go 1.21.0
