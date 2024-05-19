e0b3a47d7b8f06778cbe3b6f97cdf2ff30a3a798
LoveAnneLiser <2974728131@qq.com>
2024-05-19 12:42:55 +0800

    1.6路由实现之前缀树和测试

---
+++ zhuo/tree.go
@@ -0,0 +1,72 @@
+package zhuo
+
+import "strings"
+
+// 前缀树
+type treeNode struct {
+	name     string
+	children []*treeNode
+}
+
+// Put put path: /user/get/:id
+func (t *treeNode) Put(path string) {
+	root := t
+	strs := strings.Split(path, "/")
+	for index, name := range strs {
+		if index == 0 {
+			continue
+		}
+		children := t.children
+		isMatch := false
+		for _, node := range children {
+			if node.name == name {
+				isMatch = true
+				t = node
+				break
+			}
+		}
+		if !isMatch { // 没有匹配上
+			node := &treeNode{
+				name:     name,
+				children: make([]*treeNode, 0),
+			}
+			children = append(children, node)
+			t.children = children
+			t = node
+		}
+	}
+	t = root
+}
+
+// Get get path: /user/get/1
+func (t *treeNode) Get(path string) *treeNode {
+	strs := strings.Split(path, "/")
+	for index, name := range strs {
+		if index == 0 {
+			continue
+		}
+		children := t.children
+		isMatch := false
+		for _, node := range children {
+			if node.name == name || node.name == "*" || strings.Contains(node.name, ":") {
+				isMatch = true
+				t = node
+				if index == len(strs)-1 {
+					return node
+				}
+				break
+			}
+		}
+		if !isMatch {
+			for _, node := range children {
+				// /user/**
+				// user/get/userInfo
+				// user/aa/bb
+				if node.name == "**" {
+					return node
+				}
+			}
+		}
+	}
+	return nil
+}
---
+++ zhuo/tree_test.go
@@ -0,0 +1,29 @@
+package zhuo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTreeNode_Put(t *testing.T) {
+	root := &treeNode{name: "/", children: make([]*treeNode, 0)}
+	root.Put("/user/get/:id")
+	root.Put("/user/create/hello")
+	root.Put("/user/create/aaa")
+	root.Put("/order/create/hello")
+
+	node := root.Get("/user/get/1")
+	fmt.Println(node)
+
+	node = root.Get("/user/create/hello")
+	fmt.Println(node)
+
+	node = root.Get("/user/create/aaa")
+	fmt.Println(node)
+
+	node = root.Get("/order/create/hello")
+	fmt.Println(node)
+
+	node = root.Get("/order/hello")
+	fmt.Println(node)
+}
